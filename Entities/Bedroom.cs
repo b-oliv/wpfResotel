b39baf5803dd4750a3aa8b92596f125e0eef614c
b-oliv <manipovore@gmail.com>
2020-11-04 18:47:23 +0100

    Add mapping bedroom typebedroom

---
+++ Entities/Bedroom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRESOTEL.Entities
+{
+    [Table("bedroom")]
+    class Bedroom
+    {
+        [Key]
+        public int IdBedroom { get; set; }
+        public int IsAvailable { get; set; }
+        public int RoomNumber { get; set; }
+        public int IsDirty { get; set; }
+        public int IdTypeBedroom { get; set; }
+        public int IdOptionRooms { get; set; }
+
+    }
+}
---
+++ Entities/Typebedroom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRESOTEL.Entities
+{
+    [Table("typebedroom")]
+    class Typebedroom
+    {
+        [Key]
+        public int IdTypeBedroom { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Capacity { get; set; }
+        public int HasBaby { get; set; }
+    }
+}
